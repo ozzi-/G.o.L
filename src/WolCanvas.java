e50dc1cf2cc7cf29ecd7004b3508512fb0cbfcb4
Oussama Zgheb <ozgheb@compass-security.com>
2017-06-09 08:37:32 +0200

    moved up

---
+++ src/WolCanvas.java
@@ -0,0 +1,31 @@
+package wol;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class WolCanvas extends Canvas {
+	private static final long serialVersionUID = 1L;
+
+	int width, height, countx, county;
+	private World world;
+
+
+	WolCanvas(World world, int w, int h, int countx, int county) {
+		this.world=world;
+		setSize(width = w, height = h);
+		this.countx=countx;
+		this.county=county;
+	}
+	
+	 @Override
+	 public Dimension getPreferredSize() {
+	      return new Dimension(Settings.winx, Settings.winy);  
+	 } 
+
+	public void paint(Graphics g) {
+		for (Cell individual : world.inhabitants) {
+			g.fillRect(individual.getPosx()*Settings.lwidth, individual.getPosy()*Settings.lheight, Settings.lwidth, Settings.lheight);			
+		}
+	}
+}
